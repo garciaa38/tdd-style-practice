359106dd76104872de2183b7b22a18d7a0858ade
Logan French <Loganfrench04@gmail.com>
2023-10-11 20:16:47 -0400

    Solved number fun test

--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,11 @@
-// Your code here
+const chai = require("chai")
+const expect = chai.expect;
+const numberFun = require('../problems/number-fun')
+
+describe("numberFun", function () {
+    describe("Returns 3", function () {
+        it("Should return 3", function () {
+            expect(returnsThree().to.eql(3))
+        })
+    })
+})
